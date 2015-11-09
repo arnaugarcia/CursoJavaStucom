7fce488c38b7b4e82252eb4e92cdf93dc7b4354b
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2015-11-09 21:19:51 +0100

    Arrays
    Arrays

---
+++ DAW_07_Arrays/ex01.java
@@ -0,0 +1,11 @@
+package DAW_07_Arrays;
+public class ex01 {
+
+	public static void main(String[] args) {
+	int pares[] = new int[20];
+	for(int i=1; i<20; i++){
+		pares[i]=i*2;
+		System.out.print(pares[i] + " - ");
+		}
+	}
+}
---
+++ DAW_07_Arrays/ex02.java
@@ -0,0 +1,29 @@
+package DAW_07_Arrays;
+import java.io.*;
+public class ex02 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+		int numero[] = new int[10];
+		int positius=0, negatius=0, zeros=0;
+		for(int i=0; i<10; i++){
+			System.out.println("Introduce un número (positivo, negativo o zero)");
+			numero[i] = Integer.parseInt(buffer.readLine());
+			if(numero[i]==0){
+				zeros++;
+			}else{
+				if(numero[i]<0){
+					negatius++;
+				}else{
+					if(numero[i]>0){
+						positius++;
+					}
+				}
+			}
+		}
+		System.out.println("Numeros positius: " + positius);
+		System.out.println("Números negatius: " + negatius);
+		System.out.println("Zeros: " + zeros);
+	}
+
+}
