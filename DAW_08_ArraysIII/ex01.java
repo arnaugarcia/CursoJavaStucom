125022c9bc40f3d9c72d309417e75a0631f2a75a
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2016-01-11 20:02:41 +0100

    11/01/2016

---
+++ DAW_08_ArraysIII/ex01.java
@@ -0,0 +1,15 @@
+package DAW_08_ArraysIII;
+import java.io.*;
+public class ex01 {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+
+		} catch (Exception e) {
+			System.out.println("Error al introducir los datos");
+		}
+
+	}
+
+}
