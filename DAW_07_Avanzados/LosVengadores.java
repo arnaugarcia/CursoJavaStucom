0a464c0e28a53d741a7713f5ddf24eedd54db0ad
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2016-01-14 20:05:12 +0100

    14/01/2016

--- DAW_07_Avanzados/LosVengadores.java
+++ DAW_07_Avanzados/LosVengadores.java
@@ -5,7 +5,7 @@ public class LosVengadores {
 	public static void main(String[] args) {
 		try {
 			BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-
+			
 		} catch (Exception e) {
 			System.out.println("Error al introducir los datos");
 		}
