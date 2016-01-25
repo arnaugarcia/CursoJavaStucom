b118e273412dc5787423e841472756f3fd4c6dd7
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2016-01-25 20:44:52 +0100

    25/01/2016

--- DAW_10_Matrices/ex02.java
+++ DAW_10_Matrices/ex02.java
@@ -1,16 +1,16 @@
 package DAW_10_Matrices;
-public class ex01 {
+public class ex02 {
 
 	public static void main(String[] args) {
-		
+		int numero=1;
 		int[][] numeros = new int[5][5];
 		for(int i=0; i<numeros.length; i++){
 			for(int j=0; j<numeros.length; j++){
-				numeros[i][j]=5;
+				numeros[i][j]=numero++;
 				System.out.print(numeros[i][j]);
 			}
 		}
-		
+
 	}
 
 }
