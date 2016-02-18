8607957699c32f475c18eb0c4881e545547ce71e
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2016-02-18 19:47:43 +0100

    18/02/2016

--- DAW_07_Avanzados/CarreraHormigas.java
+++ DAW_07_Avanzados/CarreraHormigas.java
@@ -1,86 +1,59 @@
 package DAW_07_Avanzados;
 import java.io.*;
-
 public class CarreraHormigas {
-
 	public static void main(String[] args) {
-		
 		try{
-			
-			BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-							
-			System.out.println("N˙mero de hormigas:");
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));				
+			System.out.println("Número de hormigas:");
 			int numHormigas = Integer.parseInt(buffer.readLine());
-			System.out.println("Longitud de la pista (n˙mero de pos.):");
+			System.out.println("Longitud de la pista (número de posició):");
 			int numPos = Integer.parseInt(buffer.readLine());
-			
 			while (numHormigas > numPos){
-				
-				System.out.println("Error: el n˙mero de posiciones no puede ser superior al de hormigas!");
-				System.out.println("N˙mero de hormigas:");
+				System.out.println("Error: el número de posiciones no puede ser superior al de hormigas!");
+				System.out.println("Número de hormigas:");
 				numHormigas = Integer.parseInt(buffer.readLine());
-				System.out.println("Longitud de la pista (n˙mero de pos.):");
+				System.out.println("Longitud de la pista (número de pos.):");
 				numPos = Integer.parseInt(buffer.readLine());
-				
 			}
-			
 			int pista[] = new int [numPos];
-			
 			int max = 0;
 			int min = numPos;
-			
 			for (int i=1; i <= numHormigas; i++){
-				
-				System.out.println("PosiciÛn hormiga " + i + ":");
+				System.out.println("Posició formiga " + i + ":");
 				int pos = Integer.parseInt(buffer.readLine());
 				pos -= 1;
-				
 				while (pista[pos] != 0){
-					
-					System.out.println("Error: posiciÛn ocupada!");
-					System.out.println("PosiciÛn hormiga " + i + ":");
+					System.out.println("Error: posició ocupada!");
+					System.out.println("Posició formiga " + i + ":");
 					pos = Integer.parseInt(buffer.readLine());
 					pos -= 1;
-										
 				}
-				
 				pista[pos] = i;
-				
 				pos += 1;
-				
 				if ((numPos/2) > pos){
-					
 					if (((numPos/2) - pos) < min)
 						min = ((numPos/2) - pos);
 					if (((numPos/2) - pos) > max)
 						max = ((numPos/2) - pos);
-					
 				}
 				if ((numPos/2) < pos){
-					
 					if ((pos - (numPos/2)) < min)
 						min = (pos - (numPos/2));
 					if ((pos - (numPos/2)) > max)
 						max = (pos -(numPos/2));
-					
 				}				
 				if ((numPos/2) == pos){
-					
 					min = 0;
 					max = 0;
-				
 				}
 			}
-			
 			int minim = (numPos/2) - min;
 			int maxim = (numPos/2) + max;
-			
-			System.out.println("MÌnimo: " + minim);
-			System.out.println("M·ximo: " + maxim);
-			
+			System.out.println("Mínim: " + minim);
+			System.out.println("Máxim: " + maxim);
 		}
 		catch (Exception e){
-			System.out.println("Error!");
+			System.out.println("Error " + e);
 		}
 	}
 }
--- DAW_10_Matrices/BatallaNaval.java
+++ DAW_10_Matrices/BatallaNaval.java
@@ -1,17 +1,16 @@
 package DAW_10_Matrices;
 import java.io.*;
 public class BatallaNaval {
-	static String[][] tablero = new String[10][10];
+	static char[][] tablero = new char[10][10];
 	static int tamañoBarco = 0;
-	String posicion = "";
+	static char posicion = ' ';
 	static boolean posicionAcertada=false;
 	public static void main(String[] args) {
 		try {
 			BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-			vaciarTablero();	
+			vaciarTablero();
 			mostrarTablero();
-			buffer.readLine();
-			colocarBarco(2,"H");
+			colocarBarco(2,'H');
 			mostrarTablero();
 		} catch (Exception e) {
 			System.out.println("Error al introducir los datos");
@@ -30,32 +29,33 @@ public class BatallaNaval {
 	public static void vaciarTablero(){
 		for (int i=0; i<tablero.length; i++){
 			for (int j=0; j<tablero[i].length; j++){
-				tablero[i][j]="*";
+				tablero[i][j]='*';
 			}
 		}
 	}
-	public static void colocarBarco(int $tamanoBarco, String $posicion){
-		int numeroAleatorio1 = (int) Math.floor(Math.random()*(0-10+1)+10);
-		int numeroAleatorio2 = (int) Math.floor(Math.random()*(0-10+1)+10);
-		if (!($posicion.equals("H") || $posicion.equals("V"))){
+	public static void colocarBarco(int $tamanoBarco, char $posicion){
+		int numeroAleatorio1 = (int) (10*Math.random()+0);
+		int numeroAleatorio2 = (int) (10*Math.random()+0);
+		if (!($posicion=='H' || $posicion=='V')){
 			System.out.println("Error");
 		}else{
 			switch($posicion){
-			case "H":
-				while(posicionAcertada==true){
+			case 'H':
+				System.out.println("jashdfjkasdjkjf");
+				while(!posicionAcertada){
 					for(int i=0; i<$tamanoBarco; i++){
-						if(tablero[numeroAleatorio1][numeroAleatorio2+i]=="*"){
-							tablero[numeroAleatorio1][numeroAleatorio2+i]="B";
+						if(tablero[numeroAleatorio1][numeroAleatorio2+i]=='*'){
+							tablero[numeroAleatorio1][numeroAleatorio2+i]='B';
 						}else{
 							posicionAcertada=false;
 							for(int j=0; j<$tamanoBarco; j++){
-								tablero[numeroAleatorio1][numeroAleatorio2+i]="*";
+								tablero[numeroAleatorio1][numeroAleatorio2+i]='B';
 							}
 						}
 					}	
 				}
 				break;
-			case "V":
+			case 'V':
 				break;
 			}
 		}
